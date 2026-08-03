a8ad55c78964745fe44e0122563db1e4a05779f6
Roman Savosin <savosin_33@mail.ru>
2026-08-03 23:37:31 +0300

    Изменил добавления в друзья

--- src/main/java/ru/yandex/practicum/filmorate/dal/JdbcFriendDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/JdbcFriendDbStorage.java
@@ -49,7 +49,7 @@ public class JdbcFriendDbStorage implements FriendDbStorage {
             throw new ConditionsNotMetException("Заявка уже отправлена");
         }
 
-        String sql = "INSERT INTO friends (user_id, friend_id, confirmed) VALUES (?, ?, TRUE)";
+        String sql = "INSERT INTO friends (user_id, friend_id, confirmed) VALUES (?, ?, FALSE)";
         jdbc.update(sql, userId, friendId);
 
         log.info("Заявка в друзья создана: {} -> {}", userId, friendId);
